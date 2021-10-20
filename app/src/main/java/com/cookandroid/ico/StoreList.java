cd8dcc64f49ebc52995b86a7e257f6836bb2a380
songfox00 <songfox00@sookmyung.ac.kr>
2021-10-20 22:27:35 +0900

    change app_icon

---
+++ app/src/main/java/com/cookandroid/ico/StoreList.java
@@ -0,0 +1,14 @@
+package com.cookandroid.ico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoreList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_list);
+    }
+}
